99edde3bdfefe5567117fbd4bc95fae4981b20e5
Desperationis <smarttdiego@gmail.com>
2023-05-06 12:20:16 -0700

    Fix Map Collision + Add Light Demo
    Forgot to add collision to tile corners
    
    Add Light Demo; Scene is dark with the player holding a flickering torch

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField]
+    private Light2D light = null;
+
+    [SerializeField]
+    private float min = 3.0f;
+
+    [SerializeField]
+    private float max = 5.0f;
+
+    [SerializeField]
+    private float speed = 0.1f;
+
+    private void FixedUpdate()
+    {
+        float targetRadius = Random.Range(min, max);
+        float newRadius = Mathf.MoveTowards(light.pointLightOuterRadius, targetRadius, speed);
+
+        light.pointLightOuterRadius = newRadius;
+    }
+}
